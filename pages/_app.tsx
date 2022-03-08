5f7f49a4baeb62dfda8359129dfad82882170222
joe-main <qmmplusfact@gmail.com>
2022-03-08 21:58:35 +0900

    setting xdebug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps: { session, ...pageProps } }: {
+    Component: any;
+    pageProps: {
+        [x: string]: any;
+        session: any;
+    };
+}): JSX.Element {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps}/>
+    </SessionProvider>
+  )
 }
